e429db1417ebf6a0aafd462ef72ed45df64c45b6
huyoufumac <huyoufu1988@163.com>
2021-08-28 03:30:12 +0800

    1

--- main.go
+++ main.go
@@ -13,12 +13,13 @@ import (
 )
 
 func NewDocumentFromURL(url string) *goquery.Document {
+	fmt.Printf("正在加载%s\n", url)
 	client := &http.Client{}
 	request, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		panic(err)
 	}
-	request.Header.Add("User-Agent","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36")
+	request.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36")
 	client.CheckRedirect = func(req *http.Request, via []*http.Request) error {
 		return http.ErrUseLastResponse
 	}
@@ -45,7 +46,7 @@ func getProvinces(parent string, root *Region) []*Region {
 
 	doc := NewDocumentFromURL(parent)
 
-	parent=getRoot(parent)
+	parent = getRoot(parent)
 
 	//解析出省份
 	doc.Find(".provincetr ").Each(func(i int, s *goquery.Selection) {
@@ -64,22 +65,22 @@ func getProvinces(parent string, root *Region) []*Region {
 func getCities(parent string, root *Region) []*Region {
 
 	doc := NewDocumentFromURL(parent)
-	parent=getRoot(parent)
+	parent = getRoot(parent)
 	//解析出省份
 	doc.Find(".citytr ").Each(func(i int, s *goquery.Selection) {
 
-		cityName :=""
-		cityCode :=""
-		cityUrl:=""
+		cityName := ""
+		cityCode := ""
+		cityUrl := ""
 
 		s.Find("td a").Each(func(i int, s *goquery.Selection) {
-			if i%2==0 {
+			if i%2 == 0 {
 				cityCode = s.Text()
 				//获取省份的连接地址!!
 				cityUrl, _ = s.Attr("href")
 				cityUrl = parent + "/" + cityUrl
-			}else{
-				cityName=s.Text()
+			} else {
+				cityName = s.Text()
 			}
 		})
 		root.add(NewRegion(RegionType_City, cityName, cityCode, cityUrl))
@@ -89,37 +90,36 @@ func getCities(parent string, root *Region) []*Region {
 func getCounty(parent string, root *Region) []*Region {
 
 	doc := NewDocumentFromURL(parent)
-	parent=getRoot(parent)
-	if root.Name=="郑州市" {
+	parent = getRoot(parent)
+	if root.Name == "郑州市" {
 		fmt.Println("~~~~~~~")
 	}
 	//解析出省份
 	doc.Find(".countytr ").Each(func(i int, s *goquery.Selection) {
 
-		countyName :=""
-		countyCode :=""
-		countyUrl:=""
-
+		countyName := ""
+		countyCode := ""
+		countyUrl := ""
 
 		s.Find("td ").Each(func(i int, s *goquery.Selection) {
-			a:=s.Find("a")
-			if a!=nil{
+			a := s.Find("a")
+			if a.Length() > 0 {
 
-				if i%2==0 {
+				if i%2 == 0 {
 					countyCode = a.Text()
 					//获取省份的连接地址!!
 					countyUrl, _ = a.Attr("href")
 					countyUrl = parent + "/" + countyUrl
-				}else{
-					countyName=a.Text()
+				} else {
+					countyName = a.Text()
 				}
-			}else{
-				fmt.Println("没有找到a标签")
+			} else {
+				//fmt.Println("没有找到a标签")
 				//为空的话 说明没有下一级了
-				if i%2==0 {
+				if i%2 == 0 {
 					countyCode = s.Text()
-				}else{
-					countyName=s.Text()
+				} else {
+					countyName = s.Text()
 				}
 			}
 
@@ -129,12 +129,9 @@ func getCounty(parent string, root *Region) []*Region {
 	return root.Children
 }
 
-
 func main() {
 
 	url := "http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2020/index.html"
-
-
 
 	root := NewRegion(RegionType_Country, "中国", "", url)
 	provinces := getProvinces(url, root)
@@ -146,25 +143,22 @@ func main() {
 
 		//获取县
 		for _, city := range cities {
-			getCounty(city.Url,city)
+			getCounty(city.Url, city)
 		}
-
 
 	}
 
 	//fmt.Println(root)
 
-	bytes, _ := json.MarshalIndent(root,"","\t")
+	bytes, _ := json.MarshalIndent(root, "", "\t")
 
-	ioutil.WriteFile("C:\\Users\\Administrator\\Desktop\\data.json",bytes,os.ModePerm)
-
+	ioutil.WriteFile("data.json", bytes, os.ModePerm)
 
 }
 
-func getRoot(source string) string{
+func getRoot(source string) string {
 	return substring(source, 0, strings.LastIndex(source, "/"))
 }
-
 
 func substring(source string, start int, end int) string {
 	var r = []rune(source)
@@ -185,4 +179,3 @@ func substring(source string, start int, end int) string {
 
 	return substring
 }
-
